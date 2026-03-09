dd21e3e2030b91f131f9f178c93e8297746c6d94
Md Masud <mransack@gmail.com>
2026-03-10 01:17:43 +0530

    Go Workspace

--- hello/go.mod
+++ hello/go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	golang.org/x/example/hello v0.0.0-20250915201037-7f05d217867b // indirect
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-    "fmt";
- "rsc.io/quote";
- "mransack/greetings"
+	"fmt"
+	"mransack/greetings"
+
+	"golang.org/x/example/hello/reverse"
+	"rsc.io/quote"
 )
-func main(){
-    fmt.Println(quote.Go())
-    message := greetings.Hello("Maass")
-    fmt.Println(message)
+
+func main() {
+	fmt.Println(quote.Go())
+	message := greetings.Hello("Maass")
+	fmt.Println(message)
+	fmt.Println(reverse.String("Hello"))
+	fmt.Println(reverse.String("Hello"), reverse.Int(24601))
 }
